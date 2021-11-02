51a496541f1a0fdd3fbadc20bc1e25ae23f6250b
Julius Garrido <jflgarrido@gmail.com>
2021-11-01 20:02:28 -0600

    commit tree ecb8b15b56ac20db395afdafa14f895084ef1035

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Julius Garrido';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase ();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
